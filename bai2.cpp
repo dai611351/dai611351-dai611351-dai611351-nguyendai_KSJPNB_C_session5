5669be2b44c43ddfe2229351e47fd1eaa9264760
dai611351 <dai611351@gmail.com>
2024-11-26 01:38:20 +0700

    bai2

---
+++ bai2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	    int i = 10;
+     	int a;
+		while (i != a) {	
+		printf("moi ban nhap so may man:");
+		scanf ("%d", &a);
+}					
+	printf("%d la dap an chinh sac", i);
+	    return 0;
+}	
